d3d0fb63ee2ff66787e62113f72f31a0dcbd31c9
dominikdobos <dominik.dobos33@gmail.com>
2024-09-25 20:31:10 +0200

    kosár css + végösszeg

--- view/Kosar.js
+++ view/Kosar.js
@@ -17,9 +17,32 @@ export default class Kosar {
         new KosarTermek(this.#szuloElem, this.#TERMEK[index]);
         vegosszeg += this.#TERMEK[index].amount * this.#TERMEK[index].price;
       }
-      this.#szuloElem.append(`<li>${vegosszeg} Ft</li>`);
+      this.#szuloElem
+        .append(`<li class="list-group-item d-flex align-items-center gap-2 p-3">
+              <div class="fw-bold">Fizetendő:</div>
+              <p class="m-0">${vegosszeg} Ft</p>
+            </li>`);
     } else {
-      this.#szuloElem.html("A kosár üres");
+      this.#szuloElem.html(`<div
+        class="ures-kosar d-flex justify-content-center align-items-center position-fixed top-50 flex-column"
+        style="right: 20%"
+      >
+        <h2>A kosár üres!</h2>
+        <div>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="50"
+            height="50"
+            fill="currentColor"
+            class="bi bi-cart"
+            viewBox="0 0 16 16"
+          >
+            <path
+              d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5M3.102 4l1.313 7h8.17l1.313-7zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4m7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4m-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2m7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2"
+            />
+          </svg>
+        </div>
+      </div>`);
     }
   }
 }
