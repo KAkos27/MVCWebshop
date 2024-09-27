f085aaf0397fef26129ee069d7ac58c597cc4845
KAkos27 <akoskosztolanyi@gmail.com>
2024-09-27 08:55:28 +0200

    kis ez kis az

--- controller/Controller.js
+++ controller/Controller.js
@@ -10,6 +10,7 @@ export default class Controller {
   #MODEL;
   #ADMIN_LINK;
   #TERMEKEK_LINK;
+  #FOOLDAL;
 
   constructor() {
     this.initController();
@@ -22,57 +23,38 @@ export default class Controller {
     this.#KOSAR = $(".kosar");
     this.#ADMIN_LINK = $(".adminOldal");
     this.#TERMEKEK_LINK = $(".termekOldal");
+    this.#FOOLDAL = $(".foOldal");
 
     new Tartalom(this.#TARTALOM, TOYS);
     new Kosar(this.#KOSAR, KOSAR_LISTA);
   };
 
   initEsemenyek = () => {
-    this.kosarbaRak();
-    this.kosarbolTorol();
-    this.kosarNovel();
-    this.kosarCsokkent();
+    this.esemenyKezelo("kosarbaRak", this.#MODEL.kosarhozAd);
+    this.esemenyKezelo("kosarbolTorles", this.#MODEL.kosarbolTorol);
+    this.esemenyKezelo("kosarNovel", this.#MODEL.kosarhozAd);
+    this.esemenyKezelo("kosarCsokkent", this.#MODEL.kosarCsokkent);
+
     this.adminOldalKlikk();
     this.termekOldalKlikk();
   };
 
-  kosarbaRak() {
-    $(window).on("kosarbaRak", (event) => {
-      this.#MODEL.kosarhozAd(event.detail);
+  esemenyKezelo = (esemeny, esemenyEljaras) => {
+    $(window).on(esemeny, (event) => {
+      esemenyEljaras(event.detail);
       new Kosar(this.#KOSAR, KOSAR_LISTA);
     });
-  }
-
-  kosarbolTorol() {
-    $(window).on("kosarbolTorles", (event) => {
-      this.#MODEL.kosarbolTorol(event.detail);
-      new Kosar(this.#KOSAR, KOSAR_LISTA);
-    });
-  }
-
-  kosarNovel() {
-    $(window).on("kosarNovel", (event) => {
-      this.#MODEL.kosarhozAd(event.detail);
-      new Kosar(this.#KOSAR, KOSAR_LISTA);
-    });
-  }
-
-  kosarCsokkent() {
-    $(window).on("kosarCsokkent", (event) => {
-      this.#MODEL.kosarCsokkent(event.detail);
-      new Kosar(this.#KOSAR, KOSAR_LISTA);
-    });
-  }
+  };
 
   adminOldalKlikk() {
     this.#ADMIN_LINK.on("click", () => {
-      $(".foOldal").html("Admin");
+      this.#FOOLDAL.html("Admin");
     });
   }
 
   termekOldalKlikk() {
     this.#TERMEKEK_LINK.on("click", () => {
-      $(".foOldal").html(
+      this.#FOOLDAL.html(
         `<div class="tartalom row col-lg-7 col-12"></div>
         <div class="col-12 col-lg-5 position-relative border-start border-2">
           <ul class="kosar list-group list-group pt-3"></ul>
