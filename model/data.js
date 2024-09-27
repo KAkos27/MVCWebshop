aa3805431a80a10520e081363a9dae6d723ce770
KAkos27 <akoskosztolanyi@gmail.com>
2024-09-27 15:02:57 +0200

    kategorika ki

--- model/data.js
+++ model/data.js
@@ -9,7 +9,6 @@ export const TOYS = [
     description:
       "Színes flip-flop papucs formájú nyári játék kutyáknak, habszivacs töltettel, úszik a víz felszínén, ideális akciódús vízi játékokhoz, zizegő fóliával, poliészterből",
     price: 1550,
-    categories: ["playtoy"],
     amount: 1,
   },
   {
@@ -22,7 +21,6 @@ export const TOYS = [
     description:
       "Izgalmas kutyajáték, egészségre ártalmatlan anyagokból, úszik a vízben, hurokkal, kiválóan dobálható, apportírozható és rángcigáltható, szépen sípol, poliészterből és pamutból",
     price: 2890,
-    categories: ["playtoy"],
     amount: 1,
   },
   {
@@ -35,7 +33,6 @@ export const TOYS = [
     description:
       "Masszív, pattogó labda kutyáknak hőre lágyuló elasztomerből. sok réssel és nagy lyukkal, megtölthető jutalomfalatokkal, száraztáppal  vagy pasztákkal, az extra hosszú játékélményért, TPE-ből",
     price: 2450,
-    categories: ["playtoy", "feedingtoy"],
     amount: 1,
   },
   {
@@ -48,7 +45,6 @@ export const TOYS = [
     description:
       "Ízesített rágójáték kutyáknak finom bacon ízben. Rágásra ösztönzi kedvencét, bütykös felülettel az íny masszírozásához. Erősíti a fogakat és az ínyt- Fogápoló hatású, nylonból és TPU-ból.",
     price: 890,
-    categories: ["playtoy", "feedingtoy"],
     amount: 1,
   },
   {
@@ -58,7 +54,6 @@ export const TOYS = [
     description:
       "Kötél kölyköknek és kisebb kutyáknak, dobálásra és apportírozásra. A csomózott kötél közepén csonttal, noppjai rágásra ösztönöznek és stimulálják a fogínyt. Poliészterből és termoplasztikus gumiból.",
     price: 1790,
-    categories: ["playtoy"],
     amount: 1,
   },
   {
@@ -71,7 +66,6 @@ export const TOYS = [
     description:
       "Labdahajító kutyáknak, megkönnyíti a labda lehajolás nélkül történő felvételét, lehetővé teszi a messzebbre dobálást kevesebb erőfeszítéssel, Ø 6 - 6,5 cm-es labdákhoz, PP és TPR (hőre lágyuló gumi)",
     price: 1550,
-    categories: ["playtoy"],
     amount: 1,
   },
   {
@@ -84,7 +78,6 @@ export const TOYS = [
     description:
       "Rozsdamentes acél etetőtál közepén kidomborodással a gyors evés megakadályozására; akár ötödjére csökkenhet a mohó evés miatti félrenyelés, alján prémium gumigyűrű a stabilitásért; gépben elmosható.",
     price: 4550,
-    categories: ["feedingtoy"],
     amount: 1,
   },
   {
@@ -97,7 +90,6 @@ export const TOYS = [
     description:
       "Színes nyalogató szőnyeg kutyáknak, macskáknak, tökéletes pasztákhoz, segíti a lassú evést, csökkenti a stresszt, szeparációs szorongást és unalmat, fogbarát, élelmiszer-ipari minőségű szilikonból.",
     price: 1670,
-    categories: ["feedingtoy"],
     amount: 1,
   },
 ];
